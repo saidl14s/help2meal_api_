f21c4cce07fe25e448c9610dfe1a243b8a8e712d
Jesús Said Llamas Manriquez <saidllamas@SaidLlamas.local>
2019-08-30 17:49:12 -0500

    update privacy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Política de privacidad</title>
+    <meta property="og:url"                content="https://helpmeal-api.herokuapp.com/privacy" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:locale" content="es_MX" /> 
+    <meta property="og:title"              content="Help2Meal | Recetario digital" />
+    <meta property="og:description"        content="'Tú pones los ingredientes, nosotros las recetas!" />
+    <meta property="og:image"              content="https://drive.google.com/uc?export=download&id=1j1fvXq53FwAfO7bdQ7HTzFAjIW4-n9MA" />
+    <meta property="fb:app_id"             content="2700516886645245" />
 </head>
 <body>
 
