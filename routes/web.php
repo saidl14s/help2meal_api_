c1c01e397f7c36b332249584c1096b5a08a70d18
Jesús Said Llamas Manriquez <saidllamas@SaidLlamas.local>
2019-08-30 17:20:52 -0500

    add politica privacidad

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('privacy', function () {
+    return view('privacy');
+});
 
 Route::get('/', function () {
     try {
